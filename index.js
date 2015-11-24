f634be35a5f9ec2e467de2f208ef8a752cdb82d3
druken-saint <devang.only@gmail.com>
2015-11-25 01:12:50 +0530

    added tasks todo

--- index.js
+++ index.js
@@ -25,3 +25,14 @@ io.on('connection', function(socket){
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+//TODO
+/*
+    Broadcast a message to connected users when someone connects or disconnects
+    Add support for nicknames
+    Don’t send the same message to the user that sent it himself. Instead, append the message directly as soon as he presses enter.
+    Add “{user} is typing” functionality
+    Show who’s online
+    Add private messaging
+    Share your improvements!
+*/
